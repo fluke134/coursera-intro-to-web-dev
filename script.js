f90e053f4693bf44fa7e46e6b50f524e8590390d
Darryl Govender <darryl@synthesis.co.za>
2025-01-10 16:40:17 +0200

    Latest

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 // Wait for the DOM to be fully loaded
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the button element by its ID
-    const button = document.getElementById('myButton');
-
-    // Add a click event listener to the button
-    button.addEventListener('click', () => {
-        // Code to execute when the button is clicked
-        alert('Button was clicked!');
-    });
-});
+function contact() {
+    // Code to execute when the button is clicked
+    alert('Thanks for contacting me!');
+};
 
 document.getElementById('menu-toggle').addEventListener('click', function() {
     var navMenu = document.getElementById('nav-menu');
-    if (navMenu.style.display === 'block') {
+    if (navMenu.style.display === 'block' || navMenu.style.display === '') {
         navMenu.style.display = 'none';
     } else {
         navMenu.style.display = 'block';
